65bcc38dd0da01996e269cd0b9c6e48b406be211
werble7 <bravehenrique@gmail.com>
2024-11-20 10:58:28 -0300

    Adiciona outras bibliotecas, função de cliques maximais e plot do grafo

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,5 +1,7 @@
 from scipy.io import mmread
 import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
 
 # Load the matrix from the .mtx file
 matrix = mmread("../data/soc-dolphins.mtx").tocsc()  # Convert to a Compressed Sparse Column format
@@ -8,10 +10,50 @@ matrix = mmread("../data/soc-dolphins.mtx").tocsc()  # Convert to a Compressed S
 if matrix.shape[0] != matrix.shape[1]:
     raise ValueError("The matrix should be square to represent a graph adjacency matrix.")
 
-# Calculate the degree of each vertex
-# Sum the number of non-zero entries in each row for an undirected graph
-degrees = np.array(matrix.getnnz(axis=1))
+# Create a graph with nodes and edges given by .mtx file.
+graph = nx.Graph(matrix)
 
-# Print the degrees of each vertex
-for vertex, degree in enumerate(degrees):
-    print(f"Vertex {vertex}: Degree {degree}")
+# List the degrees by node
+for node, degree in graph.degree:
+    print(f"Node: {node + 1}, Degree: {degree}")
+
+# The number of maximal cliques in G
+print(f"\nThe number of maximal cliques in the graph: {sum(1 for c in nx.find_cliques(graph))}")
+
+# The largest maximal clique in G
+print(f"\nThe largest maximal clique in the graph: {max(nx.find_cliques(graph), key=len)}")
+
+# 3d spring layout
+pos = nx.spring_layout(graph, dim=3, seed=779)
+# Extract node and edge positions from the layout
+node_xyz = np.array([pos[v] for v in sorted(graph)])
+edge_xyz = np.array([(pos[u], pos[v]) for u, v in graph.edges()])
+
+# Create the 3D figure
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+
+# Plot the nodes - alpha is scaled by "depth" automatically
+ax.scatter(*node_xyz.T, s=100, ec="w")
+
+# Plot the edges
+for vizedge in edge_xyz:
+    ax.plot(*vizedge.T, color="tab:gray")
+
+
+def _format_axes(ax):
+    """Visualization options for the 3D axes."""
+    # Turn gridlines off
+    ax.grid(False)
+    # Suppress tick labels
+    for dim in (ax.xaxis, ax.yaxis, ax.zaxis):
+        dim.set_ticks([])
+    # Set axes labels
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+
+
+_format_axes(ax)
+fig.tight_layout()
+plt.show()
